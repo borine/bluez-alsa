c10db83ffea4d41bebea27658e4f36b4a1cdd3d6
borine <32966433+borine@users.noreply.github.com>
2025-05-21 12:07:43 +0100

    reduce dbus timeout to 500ms

--- test/test-utils-aplay.c
+++ test/test-utils-aplay.c
@@ -277,7 +277,8 @@ CK_START_TEST(test_play_dbus_signals) {
 				"--volume=none",
 				"-v", "-v",
 				NULL), -1);
-	spawn_terminate(&sp_ba_aplay, 1500);
+//	spawn_terminate(&sp_ba_aplay, 1500);
+	spawn_terminate(&sp_ba_aplay, 500);
 
 	char output[16384] = "";
 	ck_assert_int_gt(spawn_read(&sp_ba_aplay, NULL, 0, output, sizeof(output)), 0);
